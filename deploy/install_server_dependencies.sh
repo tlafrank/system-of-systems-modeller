b330fc21e02fbedceadce669082cc2bf29f30188
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-02 02:20:47 +1100

    Update install_server_dependencies.sh

--- deploy/install_server_dependencies.sh
+++ deploy/install_server_dependencies.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 # Install the software required on the server.
+# Must be run with privileges
+
+
+# Ensure server is up to date
+echo "System being updated"
+Apt-get -y update
+apt-get -y upgrade
+echo "System updated"
+
+#Install MySQL
+apt-get -y install mysql-server
+
+#Install NodeJS
+apt-get -y install nodejs
+
+
+
