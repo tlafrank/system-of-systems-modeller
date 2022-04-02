f9de6f630d27d5da20a0b57c8f860bcf79ee5e47
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-02 15:54:39 +1100

    Update deploy_site.sh

--- deploy/deploy_site.sh
+++ deploy/deploy_site.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
+### Development environment deployment
+
+
+
+#Nodemon install?
+
+
+### Production environment deployment (NGINX?)
 # Copy the www directory to /var/www
 cp -r ../www/ /var/
 
 #Install npm dependencies
 npm --prefix /var/www/ install
-
-#Nodemon install?
-
-
-#Option for production environment (NGINX?)
