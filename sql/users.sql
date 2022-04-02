6b3edd27fa8ad556f14abc09991121281d51e83d
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-03 09:42:21 +1000

    Create users.sql

---
+++ sql/users.sql
@@ -0,0 +1,7 @@
+CREATE USER 'sosmUser'@'localhost' IDENTIFIED WITH authentication_plugin BY 'password';
+GRANT PRIVILEGE ON sosm.* TO 'sosmUser'@'localhost';
+
+
+# A temporary user used for deploying the database
+CREATE USER 'sosmDeployer'@'localhost' IDENTIFIED BY 'dfsa4352fsafy53efdsg4et';
+GRANT PRIVILEGE ON sosm.* TO 'sosmDeployer'@'localhost';
