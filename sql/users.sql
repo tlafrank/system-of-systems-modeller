e31af864e5177ffd7af6bbd4677364a37ec9377d
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-03 00:28:45 +0000

    updated users

--- sql/users.sql
+++ sql/users.sql
@@ -2,9 +2,4 @@ CREATE USER 'sosmUser'@'localhost' IDENTIFIED WITH authentication_plugin BY 'pas
 GRANT PRIVILEGE ON sosm.* TO 'sosmUser'@'localhost';
 
 
-# A temporary user used for deploying the database
-CREATE USER 'sosmDeployer'@'localhost' IDENTIFIED BY 'dfsa4352fsafy53efdsg4et';
-GRANT PRIVILEGE ON sosm.* TO 'sosmDeployer'@'localhost';
-
-
 FLUSH PRIVILEGES;
