ac7270685cebfad7a72e23cb4054c703f3c30be3
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-03 20:13:21 +1000

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,4 +1,4 @@
-CREATE USER 'sosmUser'@'localhost' IDENTIFIED WITH authentication_plugin BY 'password';
+CREATE USER 'sosmUser'@'localhost' IDENTIFIED WITH BY 'dnek384!djrbdod836wj';
 GRANT PRIVILEGE ON sosm.* TO 'sosmUser'@'localhost';
 
 
