36026bfe92b8007a910f2f94c492d8b5b5ce7f00
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-04 11:11:22 +1000

    Update alarmSystem.sql

--- testData/alarmSystem.sql
+++ testData/alarmSystem.sql
@@ -1,3 +1,8 @@
 #Example alarm system
 
-INSERT INTO subsystems 
+INSERT INTO subsystems (name,image) VALUES ('Camera', 'camera.svg');
+INSERT INTO subsystems (name,image) VALUES ('Controller','controller.png');
+INSERT INTO subsystems (name,image) VALUES ('Reed Switch', 'reedSwitch.png');
+INSERT INTO subsystems (name,image) VALUES ();
+INSERT INTO subsystems (name,image) VALUES ();
+
