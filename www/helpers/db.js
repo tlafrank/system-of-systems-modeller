a582247944da77602ea09197aac1772d2b3c4ff8
tlafrank <43323190+tlafrank@users.noreply.github.com>
2022-04-07 10:25:07 +0000

    Fixed database auth issue by upgrading to mysql2 package

--- www/helpers/db.js
+++ www/helpers/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 
 //Database connection details
@@ -11,3 +11,28 @@ var connection = mysql.createConnection({
 })
 
 module.exports = connection;
+
+/*
+Database test
+
+var mysql      = require('mysql2');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'sosmUser',
+  password : 'dnek384!djrbdod836wj',
+  database : 'db_sosm'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution;', function (error, results, fields) {
+  if (error) {
+	  throw error;
+	} else {
+
+	console.log('The solution is: ', results[0].solution);
+	};
+});
+ 
+connection.end();
+*/
