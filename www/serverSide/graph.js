87db4d1583615f2822a0c7e56b4638192a41e96f
Trevor Lafrank <trevorlafrank@hotmail.com>
2022-04-29 15:53:29 +0000

    Fixed excluded tag operation (OR vs AND).

--- www/serverSide/graph.js
+++ www/serverSide/graph.js
@@ -66,7 +66,7 @@ exports.switch = (req,res) => {
 			excludedTags = req.body.excludedFilterTag.split(',');
 			queryString[0] += sql.format(` WHERE`);
 			excludedTags.forEach((element) => {
-				queryString[0] += sql.format(` tags NOT LIKE ? OR`, ['%' + element + '%']);
+				queryString[0] += sql.format(` tags NOT LIKE ? AND`, ['%' + element + '%']);
 			})
 			queryString[0] = queryString[0].substring(0, queryString[0].length - 3);
 			break;
