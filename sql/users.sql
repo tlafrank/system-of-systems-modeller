804ca73735db883b46dd91a9a34b8523ba1670ba
Steve Adams <steve.adams2>
2022-05-17 11:00:36 +1000

    make minor tweaks and improvements
    o Make the deploy script check for permissions and operate out of directory
    o Make users tolerant of existing user (or maybe should not drop if
        present)
    o Add a start script and parameterise the db connection
    
    o The collection of assets has been moved to a script to simplify
    updating and grabbing.

--- sql/users.sql
+++ sql/users.sql
@@ -1,5 +1,5 @@
 
-DROP USER 'sosmUser'@'localhost';
+DROP USER IF EXISTS 'sosmUser'@'localhost';
 CREATE USER 'sosmUser'@'localhost' IDENTIFIED BY 'dnRk384!djrLdo}836w:';
 GRANT ALL PRIVILEGES ON db_sosm.* TO 'sosmUser'@'localhost';
 FLUSH PRIVILEGES;
