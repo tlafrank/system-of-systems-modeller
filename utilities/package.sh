4460a98067fdf01612a73e5549082e37fda27a79
Steve Adams <steve.adams2>
2022-10-19 15:27:47 +1100

    adds export script
    SOSM is of use in an offline environment, so the web and node resources
    need to be captured and moved.
    
    To aid this, the package.sh script will zip those items up and also
    grab an update view of the repository.

---
+++ utilities/package.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Package up some online accessible assets to make them available
+# on an isolated network.
+set -e
+# move to script directory for relative references to be correct
+cd $(dirname $(readlink -f $0))
+
+# Ensure off-line assets are available
+../deploy/get_assets.sh
+
+function update_modules(){
+  # Ensure we have updated/installed npm packages
+  pushd ../www
+  npm update
+  popd
+}
+update_modules
+
+pex_dir=../package_export
+rzip=${pex_dir}/resources.zip
+
+function prepare_export_dir(){
+  mkdir -p ${pex_dir}
+  if [ -e ${rzip} ]; then
+    rm ${rzip}
+  fi
+}
+prepare_export_dir
+
+# Zip the two resources now
+zip -r ${rzip} ../www/node_modules ../www/src
+
+git_mirror_dir=${pex_dir}/git_mirror
+function make_mirror()
+{
+  if [ -d ${git_mirror_dir} ]; then
+    rm -rf ${git_mirror_dir}
+  fi
+  mkdir -p ${git_mirror_dir}
+  pushd ${git_mirror_dir}
+  url=`git remote -vv | grep origin | grep fetch`
+  url=`echo ${url} | awk '{ print $2 }'`
+  git clone ${url}
+  popd
+}
+make_mirror
+gmzip=${pex_dir}/gitmirror.zip
+zip -r ${gmzip} ${git_mirror_dir}
