1e9937b380c99f3651843b2b1ebf9fcd7bb92a0d
tlafrank <43323190+tlafrank@users.noreply.github.com>
2025-08-25 22:16:24 +1000

    Added docker

--- deploy/installServerDependencies.sh
+++ deploy/installServerDependencies.sh
@@ -70,4 +70,21 @@ if [[ $continue =~ [yY] ]]; then
   systemctl restart smbd.service
 fi
 
+read -n 1 -p "Do you wish to install and configure Docker? (y/Y): " continue
+echo ""
+if [[ $continue =~ [yY] ]]; then
+  apt-get install -y git make ca-certificates curl gnupg
+  
+  #Docker
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+  # (optional) run docker without sudo
+  sudo usermod -aG docker $USER
+  newgrp docker
+fi
