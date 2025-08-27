7b8f9c61fa7d6883d7ca195c9981e5662a288dde
tlafrank <43323190+tlafrank@users.noreply.github.com>
2025-08-27 18:07:40 +1000

    Updated to run in containers (not yet tested)

--- deploy/installServerDependencies.sh
+++ deploy/installServerDependencies.sh
@@ -16,6 +16,7 @@ fi
 read -n 1 -p "Do you wish to configure git globally? (y/Y):" continue
 echo ""
 if [[ $continue =~ [yY] ]]; then
+  apt install git
   read -p "Name (First Last): " name
   git config --global user.name "$name"
   read -p "Email: " email
@@ -73,18 +74,22 @@ fi
 read -n 1 -p "Do you wish to install and configure Docker? (y/Y): " continue
 echo ""
 if [[ $continue =~ [yY] ]]; then
-  apt-get install -y git make ca-certificates curl gnupg
+
+  apt-get install -y make ca-certificates curl  
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
   
-  #Docker
-  sudo install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-  https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
-  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  #Install docker
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
   # (optional) run docker without sudo
-  sudo usermod -aG docker $USER
-  newgrp docker
+  #sudo usermod -aG docker $USER
+  #newgrp docker
 fi
--- sql/users.sql
+++ sql/users.sql
@@ -1,5 +1,5 @@
 
 DROP USER 'sosmUser'@'localhost';
-CREATE USER 'sosmUser'@'localhost' IDENTIFIED BY 'dnRk384!djrLdo}836w:';
-GRANT ALL PRIVILEGES ON db_sosm.* TO 'sosmUser'@'localhost';
+CREATE USER 'sosmUser'@'%' IDENTIFIED BY 'dnRk384!djrLdo}836w:';
+GRANT ALL PRIVILEGES ON db_sosm.* TO 'sosmUser'@'%';
 FLUSH PRIVILEGES;
